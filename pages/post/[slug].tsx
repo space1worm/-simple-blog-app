e801de3fe836f0c53cec31f049052f3bdc4f58ed
Irakli Kverenchkhiladze <irakli_kverenchkhiladze@epam.com>
2023-01-12 12:40:29 +0400

    fixed portable text issue

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -54,31 +54,36 @@ export default function PostPage({ post }: Props) {
                 </div>
 
                 <div className='mt-10'>
-                    <PortableText
-                        dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
-                        projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
-                        content={post.body}
-                        serializers={
-                            {
-                                h1: ({ children }: any) => {
-                                    return <h1 className='text-2xl font-bold my-5'>{children}</h1>
-                                },
-                                h2: ({ children }: any) => {
-                                    return <h2 className='text-xl font-bold my-5'>{children}</h2>
-                                },
-                                li: ({ children }: any) => {
-                                    return <li className="ml-4 list-disc">
-                                        {children}
-                                    </li>
-                                },
-                                link: ({ href, children }: any) => {
-                                    return <a href={href} className="text-blue-500 hover:underline">
-                                        {children}
-                                    </a>
-                                },
+                    {post.body ? (
+                        <PortableText
+                            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+                            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+                            content={post.body}
+                            serializers={
+                                {
+                                    h1: ({ children }: any) => {
+                                        return <h1 className='text-2xl font-bold my-5'>{children}</h1>
+                                    },
+                                    h2: ({ children }: any) => {
+                                        return <h2 className='text-xl font-bold my-5'>{children}</h2>
+                                    },
+                                    li: ({ children }: any) => {
+                                        return <li className="ml-4 list-disc">
+                                            {children}
+                                        </li>
+                                    },
+                                    link: ({ href, children }: any) => {
+                                        return <a href={href} className="text-blue-500 hover:underline">
+                                            {children}
+                                        </a>
+                                    },
+                                }
                             }
-                        }
-                    />
+                        />
+                    ) : (
+                        <p>Couldn'get get data</p>
+                    )}
+
                 </div>
 
             </article>
