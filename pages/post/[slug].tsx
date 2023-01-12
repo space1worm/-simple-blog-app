363a6bf0fd354a4ea82a043635216679a843e5cf
Irakli Kverenchkhiladze <irakli_kverenchkhiladze@epam.com>
2023-01-12 12:20:01 +0400

    added ability to add comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -7,6 +7,7 @@ import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
 
 import PortableText from 'react-portable-text';
+import { useState } from 'react';
 
 interface IFormInput {
     _id: string;
@@ -20,16 +21,20 @@ interface Props {
 }
 
 export default function PostPage({ post }: Props) {
-    console.log(post._id);
+    const [submited, setSubmitted] = useState(false);
+
     const { register, handleSubmit, formState: { errors } } = useForm<IFormInput>();
     const onSubmit: SubmitHandler<IFormInput> = async (data) => {
-        console.log(data);
-        await fetch('/api/createComment', {
+
+        fetch('/api/createComment', {
             method: 'POST',
             body: JSON.stringify(data),
         }).then(() => {
-            console.log(data)
-        }).catch(err => console.log(err));
+            setSubmitted(true);
+        }).catch(err => {
+            setSubmitted(false);
+            console.log(err)
+        });
     };
 
     return (
@@ -80,58 +85,73 @@ export default function PostPage({ post }: Props) {
 
             <hr className='max-w-lg my-5 mx-auto border border-yellow-500' />
 
-
-            <form
-                onSubmit={handleSubmit(onSubmit)}
-                className='flex flex-col p-5 my-10 max-w-3xl mx-auto mb-10'
-            >
-
-                <h3 className='text-sm text-yellow-500'>Enjoyed this article?</h3>
-                <h4 className='text 3xl font-bold'>Enjoyed this article?</h4>
-                <hr className='py-3 mt-2' />
-
-                <input
-                    {...register("_id")}
-                    type="hidden"
-                    name="_id"
-                    value={post._id}
-                />
-
-                <label className='block mb-5'>
-                    <span className='text-gray-700'>Name</span>
-                    <input
-                        {...register("name", { required: true })}
-                        className='shadow border rounded py-2 px-3 form-input block w-full mt-1 ring-yellow-500'
-                        placeholder="John Appleseed"
-                        type="text" />
-                </label>
-                <label className='block mb-5'>
-                    <span className='text-gray-700'>Email</span>
-                    <input
-                        {...register("email", { required: true })}
-                        className='shadow border rounded py-2 px-3 form-input block w-full mt-1 ring-yellow-500'
-                        placeholder="John Appleseed"
-                        type="email" />
-                </label>
-                <label className='block mb-5'>
-                    <span className='text-gray-700'>Comment</span>
-                    <textarea
-                        {...register("comment", { required: true })}
-                        className='shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring'
-                        rows={8} />
-                </label>
-
-                <div className='flex flex-col p-5'>
-                    {errors.name && <span className='text-red-500'>The name field is required</span>}
-                    {errors.email && <span className='text-red-500'>The email field is required</span>}
-                    {errors.comment && <span className='text-red-500'>The comment field is required</span>}
+            {submited ? (
+                <div className='flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto'>
+                    <h3 className='text-3xl font-bold'>Thank you for submitting your comment</h3>
+                    <p>Once it has been approved it will appear below!</p>
                 </div>
+            ) :
+                (
+                    <form
+                        onSubmit={handleSubmit(onSubmit)}
+                        className='flex flex-col p-5 my-10 max-w-3xl mx-auto mb-10'
+                    >
 
-                <button className='shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer' type='submit'>Submit</button>
-            </form>
+                        <h3 className='text-sm text-yellow-500'>Enjoyed this article?</h3>
+                        <h4 className='text 3xl font-bold'>Enjoyed this article?</h4>
+                        <hr className='py-3 mt-2' />
 
+                        <input
+                            {...register("_id")}
+                            type="hidden"
+                            name="_id"
+                            value={post._id}
+                        />
 
+                        <label className='block mb-5'>
+                            <span className='text-gray-700'>Name</span>
+                            <input
+                                {...register("name", { required: true })}
+                                className='shadow border rounded py-2 px-3 form-input block w-full mt-1 ring-yellow-500'
+                                placeholder="John Appleseed"
+                                type="text" />
+                        </label>
+                        <label className='block mb-5'>
+                            <span className='text-gray-700'>Email</span>
+                            <input
+                                {...register("email", { required: true })}
+                                className='shadow border rounded py-2 px-3 form-input block w-full mt-1 ring-yellow-500'
+                                placeholder="John Appleseed"
+                                type="email" />
+                        </label>
+                        <label className='block mb-5'>
+                            <span className='text-gray-700'>Comment</span>
+                            <textarea
+                                {...register("comment", { required: true })}
+                                className='shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring'
+                                rows={8} />
+                        </label>
 
+                        <div className='flex flex-col p-5'>
+                            {errors.name && <span className='text-red-500'>The name field is required</span>}
+                            {errors.email && <span className='text-red-500'>The email field is required</span>}
+                            {errors.comment && <span className='text-red-500'>The comment field is required</span>}
+                        </div>
+
+                        <button className='shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer' type='submit'>Submit</button>
+                    </form>
+                )}
+            <div className='flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2'>
+                <h3 className='text=4xl'>Comments</h3>
+                <hr className='pb-2' />
+                {post.comments.map((comment) => {
+                    return <div key={comment._id}>
+                        <p>
+                            <span className='text-yellow-500'>{comment.name}</span>:<span>{comment.comment}</span>
+                        </p>
+                    </div>
+                })}
+            </div>
         </main>
     )
 }
@@ -169,6 +189,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
                 name,
                 image
             },
+            'comments': *[
+                _type == 'comment' &&
+                post._ref == ^._id &&
+                approved == true
+            ],
             description,
             mainImage,
             slug,
--- typings.d.ts
+++ typings.d.ts
@@ -10,6 +10,7 @@ export interface Post {
             };
         };
     };
+    comments: Comment[];
     description: string;
     mainImage: {
         asset: {
@@ -20,4 +21,20 @@ export interface Post {
         current: string;
     };
     body: [object];
+}
+
+export interface Comment {
+    approved: boolean;
+    comment: string;
+    email: string;
+    name: string;
+    post: {
+        _ref: string;
+        _type: string;
+    };
+    _createdAt: string;
+    _id: string;
+    _ref: string;
+    _type: string;
+    _updatedAt: string;
 }
